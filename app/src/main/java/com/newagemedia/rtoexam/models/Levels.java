1f97423fdb00ebfde68dc3d36a606a9e2de51ca6
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2019-12-17 14:52:31 +0100

    Implementing Levels Backend

--- app/src/main/java/com/newagemedia/rtoexam/models/Levels.java
+++ app/src/main/java/com/newagemedia/rtoexam/models/Levels.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 public class Levels implements Serializable {
 
+    //Json fields: Level name
+    //questions->each level have ~10 questions
+    //answers->4 answers, only one is correct
+    //correct answer, to compare with the selected one
+
     public String level;
 
     public Levels() {
