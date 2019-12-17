b1eb84b42a237c11001dacd0420aa136ec2c96a2
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2019-12-17 10:25:55 +0100

    First Commit

---
+++ app/src/main/java/com/newagemedia/rtoexam/MainActivity.java
@@ -0,0 +1,18 @@
+package com.newagemedia.rtoexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //main activity including practice, test, reading and settings
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
