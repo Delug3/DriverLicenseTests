8a74859b38969c13553fe231320303766ee23660
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2019-12-18 14:24:07 +0100

    Updating Quiz Activity

--- app/src/main/java/com/newagemedia/rtoexam/activities/QuizActivity.java
+++ app/src/main/java/com/newagemedia/rtoexam/activities/QuizActivity.java
@@ -7,9 +7,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.newagemedia.rtoexam.R;
+import com.newagemedia.rtoexam.models.Levels;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 
 public class QuizActivity extends AppCompatActivity {
 
+    final List<Levels> questionsAndAnswers = new ArrayList<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +24,41 @@ public class QuizActivity extends AppCompatActivity {
         TextView textViewLevelName = findViewById(R.id.text_view_quiz_level_name);
 
         String levelName;
-
+        List<String> quiz = null;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if (extras == null) {
                 levelName = null;
-
+                quiz = null;
 
             } else {
                 levelName = extras.getString("LEVEL_NAME");
+                quiz = getIntent().getStringArrayListExtra("LEVEL_QUIZ");
                 textViewLevelName.setText(levelName);
-
             }
+        }
+
+        loadQuestionAndAnswers(quiz);
+    }
+
+    //each time we call this method, will load the next question & answers
+    private void loadQuestionAndAnswers(List<String>quiz)
+    {
+        for (int i = 0; i < quiz.size(); i++) {
+
+            Levels levels = new Levels();
+            levels.question = getString("question");
+
+
+
+
+
 
 
         }
+
     }
+
+
+
 }
