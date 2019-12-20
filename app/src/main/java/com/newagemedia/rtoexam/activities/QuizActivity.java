82d92342696731d836eb680ec6f1cdf1bfb66bcc
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2019-12-20 10:31:16 +0100

    Updated Quiz Activity with new features

--- app/src/main/java/com/newagemedia/rtoexam/activities/QuizActivity.java
+++ app/src/main/java/com/newagemedia/rtoexam/activities/QuizActivity.java
@@ -10,13 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import com.newagemedia.rtoexam.R;
-import com.newagemedia.rtoexam.models.Levels;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
@@ -65,64 +63,62 @@ public class QuizActivity extends AppCompatActivity {
 
         loadNextQuestion();
 
+        //compare correct answer with answer number, if its the same, then green, not the same, red and method call
+        //green:#FF00C853
+        //red:#FFD50000
         textViewAnswerOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String answerNumber ="one";
-
-                if (answerNumber.equals(correctAnswer)) {
+                if (correctAnswer.equals("one")) {
                     textViewAnswerOne.setTextColor(Color.parseColor("#FF00C853"));
                 }
                 else{
                     textViewAnswerOne.setTextColor(Color.parseColor("#FFD50000"));
-
+                    showCorrectAnswer();
                 }
+                disableMultipleClicks();
             }
         });
 
         textViewAnswerTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String answerNumber ="two";
-
-                if (answerNumber.equals(correctAnswer)) {
+                if (correctAnswer.equals("two")) {
                     textViewAnswerTwo.setTextColor(Color.parseColor("#FF00C853"));
                 }
                 else{
                     textViewAnswerTwo.setTextColor(Color.parseColor("#FFD50000"));
-
+                    showCorrectAnswer();
                 }
+                disableMultipleClicks();
             }
         });
 
         textViewAnswerThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String answerNumber ="three";
-
-                if (answerNumber.equals(correctAnswer)) {
+                if (correctAnswer.equals("three")) {
                     textViewAnswerThree.setTextColor(Color.parseColor("#FF00C853"));
                 }
                 else{
                     textViewAnswerThree.setTextColor(Color.parseColor("#FFD50000"));
-
+                    showCorrectAnswer();
                 }
+                disableMultipleClicks();
             }
         });
 
         textViewAnswerFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String answerNumber ="four";
-
-                if (answerNumber.equals(correctAnswer)) {
+                if (correctAnswer.equals("four")) {
                     textViewAnswerFour.setTextColor(Color.parseColor("#FF00C853"));
                 }
                 else{
                     textViewAnswerFour.setTextColor(Color.parseColor("#FFD50000"));
-                   //change this
-                    textViewAnswerTwo.setTextColor(Color.parseColor("#FF00C853"));
+                    showCorrectAnswer();
                 }
+                disableMultipleClicks();
             }
         });
 
@@ -131,62 +127,103 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 loadNextQuestion();
+                enableSingleClick();
             }
         });
 
 
     }
 
-    //each time we call this method, will load the next question & answers
+    /** Values are placed in Json Array.
+     * Json Objects inside Json Array
+     * Need to retrieve object position from "quiz" json array via getJSONArray.
+     * This method is the responsible for loading new questions in the activity
+     */
     private void loadNextQuestion() {
 
-        /** Values are placed in Json Array.
-         * Json Objects inside Json Array
-         * Need to retrieve object position from "quiz" json array via getJSONArray.
-         */
         List<String> quiz = getIntent().getStringArrayListExtra("LEVEL_QUIZ");
          try {
         JSONArray jsonArray= new JSONArray(quiz);
 
-                  //Levels levels = new Levels();
+        JSONObject json_data = jsonArray.getJSONObject(questionNumber);
 
-                  JSONObject json_data = jsonArray.getJSONObject(questionNumber);
-                  /*
-                  levels.question = json_data.getString("question");
-                  levels.answer_one = json_data.getString("answer_one");
-                  levels.answer_two = json_data.getString("answer_two");
-                  levels.answer_three = json_data.getString("answer_three");
-                  levels.answer_four = json_data.getString("answer_four");
-                  levels.correctAnswer = json_data.getString("correctAnswer");
-                  */
+        String question = json_data.getString("question");
+        String answerOne = json_data.getString("answer_one");
+        String answerTwo = json_data.getString("answer_two");
+        String answerThree = json_data.getString("answer_three");
+        String answerFour = json_data.getString("answer_four");
 
-                  String question = json_data.getString("question");
-                  String answerOne = json_data.getString("answer_one");
-                  String answerTwo = json_data.getString("answer_two");
-                  String answerThree = json_data.getString("answer_three");
-                  String answerFour = json_data.getString("answer_four");
+        //making this value global in this activity for being use in others methods(showcorrectAnswer)
+        correctAnswer = json_data.getString("correct_answer");
 
-                  correctAnswer = json_data.getString("correct_answer");
-
-
-                  textViewQuestionName.setText(question);
-                  textViewAnswerOne.setText(answerOne);
-                  textViewAnswerTwo.setText(answerTwo);
-                  textViewAnswerThree.setText(answerThree);
-                  textViewAnswerFour.setText(answerFour);
-
+        loadUI(question, answerOne, answerTwo, answerThree, answerFour);
+        loadDefaultColors();
 
         } catch (JSONException e) {
         e.printStackTrace();
         }
-
          //increasing question number, every time the user click on "Next" will jump to next question
          questionNumber++;
+
+    }
+
+    //load views with data from the json array
+    private void loadUI(String question, String answerOne, String answerTwo, String answerThree, String answerFour){
+
+        textViewQuestionName.setText(question);
+        textViewAnswerOne.setText(answerOne);
+        textViewAnswerTwo.setText(answerTwo);
+        textViewAnswerThree.setText(answerThree);
+        textViewAnswerFour.setText(answerFour);
     }
 
 
+    //draw green the background and the answer icon to show the correct answer
+    private void showCorrectAnswer() {
+        switch (correctAnswer) {
+            case "one":
+                textViewAnswerOne.setTextColor(Color.parseColor("#FF00C853"));
+                break;
+            case "two":
+                textViewAnswerTwo.setTextColor(Color.parseColor("#FF00C853"));
+                break;
+            case "three":
+                textViewAnswerThree.setTextColor(Color.parseColor("#FF00C853"));
+                break;
+            case "four":
+                textViewAnswerFour.setTextColor(Color.parseColor("#FF00C853"));
+                break;
+        }
+    }
+
+    //loading default colors of both, background and icon number
+    private void loadDefaultColors(){
+
+        //set default colors every time user press Next
+        textViewAnswerOne.setTextColor(Color.BLACK);
+        textViewAnswerTwo.setTextColor(Color.BLACK);
+        textViewAnswerThree.setTextColor(Color.BLACK);
+        textViewAnswerFour.setTextColor(Color.BLACK);
 
 
 
+    }
 
+    //disabling click event of answers after choosing one, prevents multiple clicks
+    private void disableMultipleClicks(){
+
+        textViewAnswerOne.setClickable(false);
+        textViewAnswerTwo.setClickable(false);
+        textViewAnswerThree.setClickable(false);
+        textViewAnswerFour.setClickable(false);
+    }
+
+    //enabling click events of answers after pressing in Next
+    private void enableSingleClick(){
+
+        textViewAnswerOne.setClickable(true);
+        textViewAnswerTwo.setClickable(true);
+        textViewAnswerThree.setClickable(true);
+        textViewAnswerFour.setClickable(true);
+    }
 }
