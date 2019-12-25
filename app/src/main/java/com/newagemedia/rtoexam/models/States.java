3a2e86e00c3b1398d33a7e52b8002a94475ee03a
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2019-12-25 09:22:11 +0100

    Implementing States model

---
+++ app/src/main/java/com/newagemedia/rtoexam/models/States.java
@@ -0,0 +1,52 @@
+package com.newagemedia.rtoexam.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class States implements Serializable {
+
+    public String state_name;
+    public String capital_name;
+    public String state_quiz_name;
+
+    public List<String> states;
+
+    public States() {
+        this.state_name = state_name;
+        this.capital_name = capital_name;
+        this.state_quiz_name = state_quiz_name;
+        this.states = states;
+    }
+
+    public String getState_name() {
+        return state_name;
+    }
+
+    public void setState_name(String state_name) {
+        this.state_name = state_name;
+    }
+
+    public String getCapital_name() {
+        return capital_name;
+    }
+
+    public void setCapital_name(String capital_name) {
+        this.capital_name = capital_name;
+    }
+
+    public String getState_quiz_name() {
+        return state_quiz_name;
+    }
+
+    public void setState_quiz_name(String state_quiz_name) {
+        this.state_quiz_name = state_quiz_name;
+    }
+
+    public List<String> getStates() {
+        return states;
+    }
+
+    public void setStates(List<String> states) {
+        this.states = states;
+    }
+}
