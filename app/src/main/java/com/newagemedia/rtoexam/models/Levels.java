e4964c0d8d5fbe0618008a49643402c6cb6bbc4e
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2019-12-26 12:58:39 +0100

    Added new field image, renamed variables

--- app/src/main/java/com/newagemedia/rtoexam/models/Levels.java
+++ app/src/main/java/com/newagemedia/rtoexam/models/Levels.java
@@ -6,31 +6,34 @@ import java.util.List;
 public class Levels implements Serializable {
 
     //Json fields: Level name
-    //questions->each level_name have ~10 questions
-    //answers->4 answers, only one is correct
+    //questions->each level_name have ~20 questions
+    //images: load one image from url
+    //answers->3 answers(a,b,c), only one is correct
     //correct answer, to compare with the selected one
 
     public Number level_number;
     public String level_name;
     public String question;
-    public String answer_one;
-    public String answer_two;
-    public String answer_three;
-    public String answer_four;
+    public String answer_a;
+    public String answer_b;
+    public String answer_c;
+    public String answer_d;
     public String correct_answer;
+    public String image_url;
 
-    //quiz: array of objects containing the questions,answers and correct answer
+    //quiz: array of objects containing the questions,answers,image and correct answer
     public List<String> quiz;
 
     public Levels() {
-        this.level_name = level_name;
         this.level_number = level_number;
+        this.level_name = level_name;
         this.question = question;
-        this.answer_one = answer_one;
-        this.answer_two = answer_two;
-        this.answer_three = answer_three;
-        this.answer_four = answer_four;
+        this.answer_a = answer_a;
+        this.answer_b = answer_b;
+        this.answer_c = answer_c;
+        this.answer_d = answer_d;
         this.correct_answer = correct_answer;
+        this.image_url = image_url;
         this.quiz = quiz;
     }
 
@@ -58,36 +61,36 @@ public class Levels implements Serializable {
         this.question = question;
     }
 
-    public String getAnswer_one() {
-        return answer_one;
+    public String getAnswer_a() {
+        return answer_a;
     }
 
-    public void setAnswer_one(String answer_one) {
-        this.answer_one = answer_one;
+    public void setAnswer_a(String answer_a) {
+        this.answer_a = answer_a;
     }
 
-    public String getAnswer_two() {
-        return answer_two;
+    public String getAnswer_b() {
+        return answer_b;
     }
 
-    public void setAnswer_two(String answer_two) {
-        this.answer_two = answer_two;
+    public void setAnswer_b(String answer_b) {
+        this.answer_b = answer_b;
     }
 
-    public String getAnswer_three() {
-        return answer_three;
+    public String getAnswer_c() {
+        return answer_c;
     }
 
-    public void setAnswer_three(String answer_three) {
-        this.answer_three = answer_three;
+    public void setAnswer_c(String answer_c) {
+        this.answer_c = answer_c;
     }
 
-    public String getAnswer_four() {
-        return answer_four;
+    public String getAnswer_d() {
+        return answer_d;
     }
 
-    public void setAnswer_four(String answer_four) {
-        this.answer_four = answer_four;
+    public void setAnswer_d(String answer_d) {
+        this.answer_d = answer_d;
     }
 
     public String getCorrect_answer() {
@@ -96,6 +99,14 @@ public class Levels implements Serializable {
 
     public void setCorrect_answer(String correct_answer) {
         this.correct_answer = correct_answer;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
     }
 
     public List<String> getQuiz() {
