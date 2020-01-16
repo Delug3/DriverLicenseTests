22da9bcbce7c8926643b2277eccb181efcc40f02
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-01-16 13:57:51 +0100

    implemented views

--- app/src/main/java/com/driverlicense/tests/activities/practice/PracticeQuizResultsActivity.java
+++ app/src/main/java/com/driverlicense/tests/activities/practice/PracticeQuizResultsActivity.java
@@ -1,6 +1,7 @@
 package com.driverlicense.tests.activities.practice;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,15 @@ import com.driverlicense.tests.R;
 
 public class PracticeQuizResultsActivity extends AppCompatActivity {
 
+    private TextView textViewTotalNumberCorrectAnswers;
+    private TextView textViewTotalNumberIncorrectAnswers;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_results);
+
+        textViewTotalNumberCorrectAnswers = findViewById(R.id.text_view_result_correct_answers);
+        textViewTotalNumberIncorrectAnswers = findViewById(R.id.text_view_results_incorrect_answers);
 
         int totalCorrectAnswers, totalIncorrectAnswers;
         if (savedInstanceState == null) {
@@ -22,7 +29,8 @@ public class PracticeQuizResultsActivity extends AppCompatActivity {
                 totalCorrectAnswers = extras.getInt("TOTAL_CORRECT_ANSWERS");
                 totalIncorrectAnswers = extras.getInt("TOTAL_INCORRECT_ANSWERS");
 
-
+                textViewTotalNumberCorrectAnswers.setText(String.valueOf(totalCorrectAnswers));
+                textViewTotalNumberIncorrectAnswers.setText(String.valueOf(totalIncorrectAnswers));
             }
         }
     }
