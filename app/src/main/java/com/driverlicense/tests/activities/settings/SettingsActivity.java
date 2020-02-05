e192124bff1f0707a20ca261b6c61ed6c13422f2
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-02-05 11:14:40 +0100

    added state selection feature

--- app/src/main/java/com/driverlicense/tests/activities/settings/SettingsActivity.java
+++ app/src/main/java/com/driverlicense/tests/activities/settings/SettingsActivity.java
@@ -1,23 +1,42 @@
 package com.driverlicense.tests.activities.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.driverlicense.tests.R;
+import com.driverlicense.tests.activities.core.MainMenuActivity;
+import com.driverlicense.tests.activities.states.StatesListActivity;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ConstraintLayout constraintLayoutSettingsStateSelection;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         configureToolbar();
+
+        constraintLayoutSettingsStateSelection = findViewById(R.id.constraint_layout_settings_state_selection);
+
+        constraintLayoutSettingsStateSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SettingsActivity.this, StatesListActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 
 
