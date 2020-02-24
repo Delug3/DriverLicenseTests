68e72b69737432b6b8c52f3422187bcc299a2853
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-02-24 12:31:51 +0100

    created and implemented Myglobals class with checkconnectivity method

---
+++ app/src/main/java/com/driverlicense/tests/activities/core/MyGlobals.java
@@ -0,0 +1,38 @@
+package com.driverlicense.tests.activities.core;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+import static com.parse.Parse.getApplicationContext;
+
+public class MyGlobals {
+
+    private Context mContext;
+
+    public MyGlobals(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public BroadcastReceiver mConnReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            boolean noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+            String reason = intent.getStringExtra(ConnectivityManager.EXTRA_REASON);
+            boolean isFailover = intent.getBooleanExtra(ConnectivityManager.EXTRA_IS_FAILOVER, false);
+
+            NetworkInfo currentNetworkInfo = intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
+            NetworkInfo otherNetworkInfo = intent.getParcelableExtra(ConnectivityManager.EXTRA_OTHER_NETWORK_INFO);
+
+            if(currentNetworkInfo.isConnected()){
+                Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "Not Connected", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+
+}
