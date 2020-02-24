cc82dc6852f7d8c150b0a6e3a3432e3f1acb7f4d
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-02-24 13:39:30 +0100

    changes to checkConnectivityChanges method

--- app/src/main/java/com/driverlicense/tests/activities/core/MyGlobals.java
+++ app/src/main/java/com/driverlicense/tests/activities/core/MyGlobals.java
@@ -1,5 +1,6 @@
 package com.driverlicense.tests.activities.core;
 
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,11 +9,14 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.widget.Toast;
 
+import com.driverlicense.tests.R;
+
 import static com.parse.Parse.getApplicationContext;
 
 public class MyGlobals {
 
     private Context mContext;
+    private Dialog connectivityDialog;
 
     public MyGlobals(Context mContext) {
         this.mContext = mContext;
@@ -34,7 +38,12 @@ public class MyGlobals {
             NetworkInfo otherNetworkInfo = intent.getParcelableExtra(ConnectivityManager.EXTRA_OTHER_NETWORK_INFO);
 
             if(!currentNetworkInfo.isConnected()){
-                Toast.makeText(getApplicationContext(), "Not Connected", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+            }else{
+                connectivityDialog = new Dialog(mContext);
+                connectivityDialog.setContentView(R.layout.activity_connectivity_changes);
+                connectivityDialog.setCancelable(false);
+                connectivityDialog.show();
             }
         }
     };
