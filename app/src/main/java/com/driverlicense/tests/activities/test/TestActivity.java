5bffc4608f8f2a14395df15397d5675ae4eed54d
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-03-10 12:39:24 +0100

    created test activity

--- app/src/main/java/com/driverlicense/tests/activities/test/TestActivity.java
+++ app/src/main/java/com/driverlicense/tests/activities/test/TestActivity.java
@@ -1,10 +1,18 @@
 package com.driverlicense.tests.activities.test;
 
-public class TestActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.driverlicense.tests.R;
+
+public class TestActivity extends AppCompatActivity {
     //  Random rand = new Random();
    // int randomIndex = rand.nextInt(result.size());
-
-
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
 }
