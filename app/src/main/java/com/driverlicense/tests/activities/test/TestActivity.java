53208dc76930b703c165c4f4dbe160055450f508
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-03-10 12:32:44 +0100

    created test activity, random questions

---
+++ app/src/main/java/com/driverlicense/tests/activities/test/TestActivity.java
@@ -0,0 +1,10 @@
+package com.driverlicense.tests.activities.test;
+
+public class TestActivity {
+    //  Random rand = new Random();
+   // int randomIndex = rand.nextInt(result.size());
+
+
+
+
+}
