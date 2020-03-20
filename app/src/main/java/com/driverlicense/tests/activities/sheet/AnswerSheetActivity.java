01278888ed91e666b99edb3f0eb395087fcad1e0
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-03-20 13:40:10 +0100

    implemented sheet activity

--- app/src/main/java/com/driverlicense/tests/activities/sheet/AnswerSheetActivity.java
+++ app/src/main/java/com/driverlicense/tests/activities/sheet/AnswerSheetActivity.java
@@ -1,35 +1,56 @@
 package com.driverlicense.tests.activities.sheet;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.driverlicense.tests.models.SavedAnswers;
+import com.driverlicense.tests.R;
+import com.driverlicense.tests.adapters.AnswerSheetAdapter;
+import com.driverlicense.tests.models.Sheet;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class AnswerSheetActivity extends AppCompatActivity {
-    private List<SavedAnswers> savedAnswersList;
+public class AnswerSheetActivity extends AppCompatActivity{
+    RecyclerView recyclerViewAnswerSheet;
+    private AnswerSheetAdapter answerSheetAdapter;
+    private List<Sheet> sheetList = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_answer_sheet);
        if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if (extras == null) {
-                savedAnswersList = null;
+                sheetList = null;
             } else {
-                savedAnswersList = extras.getParcelableArrayList("SAVED_ANSWERS");
-
+                sheetList = extras.getParcelableArrayList("SAVED_ANSWERS");
             }
 
         }
+        initViews();
+        setUpRecyclerView();
+
         //SharedPreferences sharedPref = getSharedPreferences("test_prefs", Activity.MODE_PRIVATE);
         //String saved = sharedPref.getStringSet("SAVED_ANSWERS", null);
     }
 
+    private void initViews() {
+        recyclerViewAnswerSheet = findViewById(R.id.recycler_view_answer_sheet);
+    }
+
+
+    private void setUpRecyclerView() {
+        recyclerViewAnswerSheet.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewAnswerSheet.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerViewAnswerSheet.setHasFixedSize(true);
+        answerSheetAdapter = new AnswerSheetAdapter(this, sheetList);
+        recyclerViewAnswerSheet.setAdapter(answerSheetAdapter);
+
+    }
 }
